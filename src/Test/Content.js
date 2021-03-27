b330874b505b090afd78f591b841ce5efcd44054
ArtemZan <arttema9@gmail.com>
2021-03-27 22:59:21 +0200

    Selectable answers and answers checking

--- src/Test/Content.js
+++ src/Test/Content.js
@@ -2,17 +2,25 @@ import React from 'react'
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import { Button } from '../Components'
 
+var correctAnswers = []
+
 class Question extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = { selected: [] };
+    }
+
+    componentDidMount() {
         if (this.props.correct instanceof Array) {
-            this.state = { selected: [] };
+            let correct = [];
+            for (let i = 0; i < this.props.options.length; i++)
+                correct.push(this.props.correct.find(el => el === i) !== undefined);
+            correctAnswers.push(correct);
         }
         else {
-            this.state = { selected: -1 };
+            correctAnswers.push(this.props.correct);
         }
-
     }
 
     render() {
@@ -22,7 +30,7 @@ class Question extends React.Component {
             options.push(
                 <div className="option" key={i} onClick={() => { this.OnClick(i) }}>
                     <div className="checkbox" checked={false}>
-                        <div style={this.CheckboxStyle(i)} />
+                        <div style={this.GetOptionStyle(i)} />
                     </div>
                     <h3>{this.props.options[i]}</h3>
                 </div>
@@ -30,7 +38,7 @@ class Question extends React.Component {
         }
 
         return (
-            <div className="question" value={this.state.selected}>
+            <div className="question" value={JSON.stringify(this.state.selected)}>
                 <h2>{this.props.question}</h2>
                 {options}
             </div>
@@ -39,39 +47,93 @@ class Question extends React.Component {
 
     OnClick(id) {
         let selected = this.state.selected;
+        console.log(selected);
         if (this.props.correct instanceof Array) {
-            let select = true;
-            for (let i = 0; i < selected.length; i++) {
-                if (selected[i] === id) {
-                    selected.splice(i, 1);
-                    select = false;
-                }
-            }
-
-            if (select)
-                selected.push(id);
+            selected[id] = !selected[id];
         }
-        else {
-            if (selected === id) {
+        else
+        {
+            if(selected === id)
+            {
                 selected = -1;
             }
-            else {
+            else
+            {
                 selected = id;
             }
         }
+
+
         this.setState({ selected: selected });
     }
 
-    CheckboxStyle(id) {
-        if ((this.props.correct instanceof Array && this.state.selected.find(el => id === el) !== undefined) || this.state.selected === id) {
-            return { display: "block" };
+    GetOptionStyle(id)
+    {
+        if(this.state.selected instanceof Array)
+        {
+            return { display: this.state.selected[id] ? "block" : "none" };
         }
-        return { display: "none" };
+        else
+        {
+            return { display: this.state.selected === id ? "block" : "none" }
+        }
     }
 }
 
 function CheckAnswers() {
-    /* TO DO */
+    console.log(correctAnswers);
+
+    let content = document.getElementsByClassName("content")[0];
+    content.removeChild(document.getElementById("checkAnswers"));
+
+    let mistake = false;
+
+    let questions = content.querySelectorAll(".question");
+    for (let q = 0; q < questions.length; q++) {
+
+        mistake = false;
+
+        let options = questions[q].getElementsByClassName("option");
+
+        let answer = JSON.parse(questions[q].getAttribute("value"));
+
+        if (correctAnswers[q] instanceof Array) {
+
+            for (let op = 0; op < correctAnswers[q].length; op++) {
+
+                if (answer[op]) {
+                    if (correctAnswers[q][op]) 
+                    {
+                        options[op].style.backgroundColor = "#77ff55";
+                    }
+                    else
+                    {
+                        options[op].style.backgroundColor = "#ee9090";
+                        mistake = true;
+                    }
+                }
+                else if (correctAnswers[q][op]) {
+                    options[op].style.backgroundColor = "#77ff55";
+                    mistake = true;
+                }
+            }
+        }
+        else {
+            options[correctAnswers[q]].style.backgroundColor = "#77ff55";
+
+            if(answer !== correctAnswers[q])
+            {
+                console.log(options, answer);
+                options[answer].style.backgroundColor = "#ee7070";
+                mistake = true;
+            }
+        }
+
+        if (mistake) {
+            questions[q].style.backgroundColor = "#ffcccc";
+        }
+    }
+
 }
 
 function Content() {
@@ -84,7 +146,7 @@ function Content() {
                         <Question question="How?" options={["somehow", "no", "cucumber"]} correct={2} />
                     </>
                 } />
-                <Button name="Провери отговорите" height="50px" onClick={CheckAnswers} />
+                <Button id="checkAnswers" name="Провери отговорите" height="50px" onClick={CheckAnswers} />
             </Router>
         </div>
     );
