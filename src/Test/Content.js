12f39a526a02dec2b37db6731bfc419ba9aa4735
ArtemZan <arttema9@gmail.com>
2021-03-28 18:15:32 +0300

    Basic free-answer questions

--- src/Test/Content.js
+++ src/Test/Content.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import { Button } from '../Components'
+import { GetUrl, GetHeader, lessonsUrls, GetNext } from '../LessonsRelUrl'
 
 var correctAnswers = []
+var turnedIn = false;
 
 class Question extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { selected: [] };
+        if (this.props.correct instanceof Array)
+            this.state = { selected: [] };
+        else
+            this.state = { selected: -1 };
     }
 
     componentDidMount() {
@@ -38,7 +43,7 @@ class Question extends React.Component {
         }
 
         return (
-            <div className="question" givenAnswer={JSON.stringify(this.state.selected)}>
+            <div className="question" given_answer={JSON.stringify(this.state.selected)}>
                 <h2>{this.props.question}</h2>
                 {options}
             </div>
@@ -46,95 +51,216 @@ class Question extends React.Component {
     }
 
     OnClick(id) {
-        let selected = this.state.selected;
-        console.log(selected);
-        if (this.props.correct instanceof Array) {
-            selected[id] = !selected[id];
-        }
-        else
-        {
-            if(selected === id)
-            {
-                selected = -1;
+        if (!turnedIn) {
+            let selected = this.state.selected;
+            if (this.props.correct instanceof Array) {
+                selected[id] = !selected[id];
             }
-            else
-            {
-                selected = id;
+            else {
+                if (selected === id) {
+                    selected = -1;
+                }
+                else {
+                    selected = id;
+                }
             }
-        }
 
 
-        this.setState({ selected: selected });
+            this.setState({ selected: selected });
+        }
     }
 
-    GetOptionStyle(id)
-    {
-        if(this.state.selected instanceof Array)
-        {
+    GetOptionStyle(id) {
+        if (this.state.selected instanceof Array) {
             return { display: this.state.selected[id] ? "block" : "none" };
         }
-        else
-        {
+        else {
             return { display: this.state.selected === id ? "block" : "none" }
         }
     }
 }
 
+class FreeAnswerQuestion extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        if (this.props.number !== undefined) {
+            if (this.props.precision !== undefined) {
+                correctAnswers.push({
+                    answer: Math.round(this.props.correct * Math.pow(10, this.props.precision)) / Math.pow(10, this.props.precision),
+                    precision: this.props.precision
+                });
+            }
+            else {
+                correctAnswers.push({
+                    answer: Math.round(this.props.correct),
+                    precision: 0
+                });
+            }
+        }
+        else {
+            correctAnswers.push(this.props.correct.toLowerCase());
+        }
+    }
+
+    render() {
+        return (
+            <div className="question free-answer">
+                <h2>{this.props.question}</h2>
+                <input placeholder="Въведете отговора си" type={this.props.number === undefined ? "text" : "number"}></input>
+                <div className = "correctAnswer"></div>
+            </div>
+        );
+    }
+}
+
 function CheckAnswers() {
-    console.log(correctAnswers);
+    let result = 0;
+    let maxResult = 0;
+
+    turnedIn = true;
 
     let content = document.getElementsByClassName("content")[0];
     document.getElementById("checkAnswers").style.display = "none";
-    document.getElementById("tryAgain").style.display = "inline-flex";
     document.getElementById("toLesson").style.display = "inline-flex";
 
-    let mistake = false;
+    let toNextLesson = document.getElementById("toNextLesson")
+    toNextLesson.style.display = "inline-flex";
+    let next = GetNext(window.location.pathname);
+    if (next === undefined) {
+        let topic = JSON.parse(sessionStorage.getItem("topic"));
+        toNextLesson.getElementsByClassName("button-content")[0].innerHTML = "Към " + topic[0];
+        toNextLesson.setAttribute("href", "/topics/" + topic[1]);
+    }
+    else {
+        toNextLesson.getElementsByClassName("button-content")[0].innerHTML = "Към следващия урок (" + next[0] + ')';
+        toNextLesson.setAttribute("href", "/lessons/" + next[1]);
+    }
 
     let questions = content.querySelectorAll(".question");
     for (let q = 0; q < questions.length; q++) {
 
-        mistake = false;
+        if (questions[q].classList.contains("free-answer")) {
+            //Free answer questions
+            maxResult += 3;
+            let input = questions[q].getElementsByTagName("input")[0];
+            let mistake = 0;
+            let dRes = 0;
 
-        let answerOptions = questions[q].getElementsByClassName("option");
+            console.log(input.value, correctAnswers[q]);
 
-        let givenAnswer = JSON.parse(questions[q].getAttribute("givenAnswer"));
-
-        if (correctAnswers[q] instanceof Array) {
-
-            for (let op = 0; op < correctAnswers[q].length; op++) {
-
-                if (givenAnswer[op]) {
-                    if (correctAnswers[q][op]) 
-                    {
-                        answerOptions[op].style.backgroundColor = "#77ff55";
-                    }
-                    else
-                    {
-                        answerOptions[op].style.backgroundColor = "#ee9090";
-                        mistake = true;
+            if (input.type === "number") {
+                if (Math.round(input.value * Math.pow(10, correctAnswers[q].precision)) / Math.pow(10, correctAnswers[q].precision) === correctAnswers[q].answer)
+                    dRes = 1;
+                else {
+                    dRes = 0;
+                    mistake = 2;
+                }
+            }
+            else {
+                dRes = 3;
+                let offset = 0;
+                let value = input.value.toLowerCase();
+                for (let i = 0; i < value.length && i < correctAnswers[q].length; i++) {
+                    if (value[i + offset] !== correctAnswers[q][i]) {
+                        dRes -= 1.5;
+                        mistake = 1;
+                        if (dRes === 0) {
+                            mistake = 2;
+                            break;
+                        }
                     }
                 }
-                else if (correctAnswers[q][op]) {
-                    answerOptions[op].style.backgroundColor = "#77ff55";
-                    mistake = true;
-                }
+
+            }
+            result += dRes;
+            if (mistake === 0)
+            {
+                questions[q].style.backgroundColor = "#ddffbb";
+                questions[q].getElementsByTagName("input")[0].style.backgroundColor = "#77ff55";
+            }
+            if (mistake === 1) {
+                questions[q].style.backgroundColor = "#ffe0c0";
+                let correctAnswerField = questions[q].getElementsByClassName("correctAnswer")[0];
+                correctAnswerField.innerHTML = "Правописна грешка. Правилен отговор: " + correctAnswers[q];
+                correctAnswerField.style.display = "block";
+            }
+            if (mistake === 2) {
+                questions[q].style.backgroundColor = "#ee7070";
+                let correctAnswerField = questions[q].getElementsByClassName("correctAnswer")[0];
+                correctAnswerField.innerHTML = correctAnswers[q];
+                correctAnswerField.style.display = "block";
             }
         }
         else {
-            answerOptions[correctAnswers[q]].style.backgroundColor = "#77ff55";
+            let mistake = false;
 
-            if(givenAnswer !== correctAnswers[q])
+            let correctOptions = 0;
+            for (let i = 0; i < correctAnswers[q].length; i++) {
+                correctOptions += correctAnswers[q][i];
+            }
+
+            let answerOptions = questions[q].getElementsByClassName("option");
+
+            let givenAnswer = JSON.parse(questions[q].getAttribute("given_answer"));
+
+            if (correctAnswers[q] instanceof Array) {
+                maxResult += 2;
+
+                let dRes = 0;
+
+                for (let op = 0; op < correctAnswers[q].length; op++) {
+
+                    if (givenAnswer[op]) {
+                        if (correctAnswers[q][op]) {
+                            answerOptions[op].style.backgroundColor = "#77ff55";
+                            dRes += 2 / correctOptions;
+                        }
+                        else {
+                            answerOptions[op].style.backgroundColor = "#ee7070";
+                            dRes -= 2 / correctOptions;
+                            mistake = true;
+                        }
+                    }
+                    else if (correctAnswers[q][op]) {
+                        answerOptions[op].style.backgroundColor = "#77ff55";
+                        mistake = true;
+                    }
+                }
+
+                if (dRes > 0)
+                    result += dRes;
+            }
+            else {
+                maxResult += 1;
+
+                answerOptions[correctAnswers[q]].style.backgroundColor = "#77ff55";
+
+                if (givenAnswer === -1)
+                    mistake = true;
+                else
+                    if (givenAnswer !== correctAnswers[q]) {
+                        answerOptions[givenAnswer].style.backgroundColor = "#ee7070";
+                        mistake = true;
+                    }
+                    else
+                        result += 1;
+            }
+
+            if (mistake) {
+                questions[q].style.backgroundColor = "#ffcccc";
+            }
+            else
             {
-                console.log(answerOptions, givenAnswer);
-                answerOptions[givenAnswer].style.backgroundColor = "#ee7070";
-                mistake = true;
+                questions[q].style.backgroundColor = "#ddffbb";
             }
         }
-
-        if (mistake) {
-            questions[q].style.backgroundColor = "#ffcccc";
-        }
     }
+
+
+    document.getElementById("result").innerHTML = "Резултат: " + Math.round(result / maxResult * 1000) / 10 + "%";
 }
 
 function Content() {
@@ -145,11 +271,13 @@ function Content() {
                     <>
                         <Question question="What?" options={["dk", "that", "something"]} correct={[2, 0]} />
                         <Question question="How?" options={["somehow", "no", "cucumber"]} correct={2} />
+                        <FreeAnswerQuestion question="Explain" correct="Because" />
                     </>
                 } />
+                <h2 id="result" style={{ width: "100%", textAlign: "center" }}></h2>
                 <Button id="checkAnswers" name="Провери отговорите" height="50px" onClick={CheckAnswers} />
-                <Button id="tryAgain" name="Пробвай отново" height="50px" link = {window.location.pathname}/>
-                <Button id="toLesson" name="Назад към урока" height="50px" link = {window.location.pathname.replace("tests", "lessons")}/>
+                <Button id="toLesson" name="Назад към урока" height="50px" link={window.location.pathname.replace("tests", "lessons")} />
+                <Button id="toNextLesson" height="50px" link="#" />
             </Router>
         </div>
     );
--- src/Topics/Topics.js
+++ src/Topics/Topics.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {Route, BrowserRouter as Router} from 'react-router-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 
 import Content from './Content'
 import { Footer } from '../Components'
-import { DefaultNavbar as Navbar, Header} from '../Components'
+import { DefaultNavbar as Navbar, Header } from '../Components'
 
 if (window.location.pathname.match("/topics/")) {
     import('./Content.css');
@@ -13,12 +13,30 @@ function Subjects() {
     return (
         <>
             <Router>
-                <Route path="/topics/anatomy_and_physiology" exact component={() => <Header content="Анатомия и физиология" />} />
-                <Route path="/topics/cytology" exact component={() => <Header content="Клетка" />} />
-                <Route path="/topics/viruses" exact component={() => <Header content="Вируси" />} />
-                <Route path="/topics/genetics" exact component={() => <Header content="Генетика" />} />
-                <Route path="/topics/ecology" exact component={() => <Header content="Екология" />} />
-                <Route path="/topics/chemistry" exact component={() => <Header content="Биохимия" />} />
+                <Route path="/topics/anatomy_and_physiology" exact component={() => {
+                    sessionStorage.setItem("topic", JSON.stringify(["Анатомия и физиология", "anatomy_and_physiology"]));
+                    return <Header content="Анатомия и физиология" />
+                }} />
+                <Route path="/topics/cytology" exact component={() => {
+                    sessionStorage.setItem("topic", "Клетка");
+                    return <Header content="Клетка" />
+                }} />
+                <Route path="/topics/viruses" exact component={() => {
+                    sessionStorage.setItem("topic", "Вируси");
+                    return <Header content="Вируси" />
+                }} />
+                <Route path="/topics/genetics" exact component={() => {
+                    sessionStorage.setItem("topic", "Генетика");
+                    return <Header content="Генетика" />
+                }} />
+                <Route path="/topics/ecology" exact component={() => {
+                    sessionStorage.setItem("topic", "Екология");
+                    return <Header content="Екология" />
+                }} />
+                <Route path="/topics/chemistry" exact component={() => {
+                    sessionStorage.setItem("topic", "Биохимия");
+                    return <Header content="Биохимия" />
+                }} />
             </Router>
 
             <Navbar />
