bd8abd2bc2954672d29a6d2fdf7f19971191c09d
ArtemZan <arttema9@gmail.com>
2021-03-31 14:39:00 +0300

    Dynamically loading video

--- src/Lesson/Content.js
+++ src/Lesson/Content.js
@@ -2,42 +2,35 @@ import { Button } from '../Components'
 import React from "react"
 
 const mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';//wtf is this
+const pngCodec = 'image/png';
 
 class Content extends React.Component {
-
     render() {
+        console.log("render");
         return (
             <div className="content">
-                <video>
-                    <source id="videoSource" type="video/mp4"></source>
+                <video id = "video" controls>
                 </video>
-                <Button onClick={() => {
-                    console.log(document.getElementById("videoSource").src);
-                    fetch(document.getElementById("videoSource").src).then(blob => {blob.text().then(res=>{console.log(res)})})
-                }} name="press this button to break dev tools :)" />
             </div>
         );
     }
 
     componentDidMount() {
-        var videoNode = document.getElementById("videoSource");
-
-        videoNode.src = this.videoUrl;
+        var videoNode = document.getElementById("video");
 
         var request = new XMLHttpRequest();
-        request.open("GET", "/video1.mp4");
+        request.open("GET", "/video2.mp4");
         request.responseType = 'arraybuffer';
 
         request.onload = function () {
             var array = new Array(request.response);
-            var blob = new Blob(array, {type: mimeCodec});
+            var blob = new Blob(array, { type: mimeCodec });
             console.log(blob)
+
             videoNode.src = URL.createObjectURL(blob);
         }
 
         request.send();
-
-
     }
 }
 
