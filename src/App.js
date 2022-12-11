491d53e69fb515ab1258e2367c16aeb180985c1f
ArtemZankovskiy <artem.zankovskiy@infinno.eu>
2022-12-11 14:49:43 +0200

    Removed authorixation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import { React, StrictMode, useEffect } from 'react';
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom"
-import { useState } from "react"
+import { React, StrictMode, useContext } from 'react';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 
 import HomePage from './pages/HomePage/HomePage'
 import Universities from './pages/Universities/Universities'
 import LessonsNav from './pages/LessonsNav/LessonsNav'
 import Lesson from './pages/Lesson/Lesson'
-import { Login, Signup } from './pages/Auth/Auth'
 import Test from './pages/Test/Test'
 import Terms from './pages/Legality/Terms';
 import NotFound from './pages/NotFound/NotFound';
@@ -16,7 +14,6 @@ import './Style/Style.css'
 
 import { CookiesAcceptedProvider, cookiesContext, ThemeProvider } from './Utilities';
 import { CookiesWindow } from './Components';
-import { useContext } from 'react/cjs/react.development';
 
 
 function CookiesWindowOrApp() {
@@ -27,18 +24,16 @@ function CookiesWindowOrApp() {
   return (
     accepted ?
       <Router forceRefresh={false}>
-        <Switch>
+        <Routes>
           <Route path="/" exact component={HomePage} />
           <Route path="/universities" exact component={Universities} />
           <Route path="/lessons" exact component={LessonsNav} />
           <Route path="/lessons/*/" exact component={Lesson} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/signup" exact component={Signup} />
           <Route path="/tests/*/" exact component={Test} />
           <Route path="/terms-and-conditions" exact component={Terms} />
           <Route path="/privacy-policy" exact component={Privacy} />
           <Route path="/*" component={NotFound} />
-        </Switch>
+        </Routes>
       </Router>
       :
       <CookiesWindow />
